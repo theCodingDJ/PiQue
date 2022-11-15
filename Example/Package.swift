7cd9496851723bda50620cf253a212232425a06e
Lyubomir Marinov <Lyubomir.Marinov@Tryzens.com>
2022-11-15 04:47:54 +0200

    Update swift tools version to match the github actions latest.

--- Example/Package.swift
+++ Example/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.5
 import PackageDescription
 
 /// Used as a workaround to exclude the `Example` app sources from the package tree in the Project Navigator.
